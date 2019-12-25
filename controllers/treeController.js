583d51994825fbe40e20435d9f9eda0ceff10bc1
Michelle <msumalini@gmail.com>
2019-12-25 09:29:17 +0530

    Modified the retrieveTreeResults function to query tree documents by fields that have array of values

--- controllers/treeController.js
+++ controllers/treeController.js
@@ -19,18 +19,42 @@ function retrieveAllTrees(){
 
 function retrieveTreeResults(filterObject){
 
-    if(filterObject.has("dbh")){
-
+    if("dbh" in filterObject){
+        var querydbh=filterObject.dbh;
+        filterObject["dbh.0"]={"$lte":querydbh}
+        filterObject["dbh.1"]={"$gte":querydbh}
+        delete filterObject["dbh"]
     }
-    if(filterObject.has("height")){
 
+    if("height" in filterObject){
+        var queryheight=filterObject.height;
+        filterObject["height.0"]={"$lte":queryheight}
+        filterObject["height.1"]={"$gte":queryheight}
+        delete filterObject["height"]
     }
+
+    if("use" in filterObject){
+        var queryuse=filterObject.use;
+        filterObject["use"]={"$in":[queryuse]}
+    }
+
+    if("tree_size" in filterObject){
+        var querytree_size=filterObject.tree_size;
+        filterObject["tree_size"]={"$in":[querytree_size]}
+    }
+
+    if("taxonomic_status" in filterObject){
+        var querytaxonomic_status=filterObject.taxonomic_status;
+        filterObject["taxonomic_status"]={"$in":[querytaxonomic_status]}
+    }
+
     var promise = new Promise(function(resolve,reject){
         Tree.find(filterObject,function(error,docs){
             if(error){
                 reject(error)
             }
             else{
+                console.log(docs)
                 resolve(docs)
             }
         })

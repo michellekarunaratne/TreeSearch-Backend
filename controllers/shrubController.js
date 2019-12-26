b9fabb39aed83579fb01edaf1164ef4ce1190124
Michelle <msumalini@gmail.com>
2019-12-26 15:24:59 +0530

    fixed the logical error when quering the height in trees and shrubs

--- controllers/shrubController.js
+++ controllers/shrubController.js
@@ -24,7 +24,7 @@ function retrieveShrubResults(filterObject){
        // filterObject["height.0"]={"$lte":queryheight}
        // filterObject["height.1"]={"$gte":queryheight}
        // delete filterObject["height"]
-       filterObject["max_height"]={$gte:queryheight[0]}
+       filterObject["$and"]=[{"max_height":{"$gte":queryheight[0]}},{"max_height":{"$lte":queryheight[1]}}]
        delete filterObject["height"]
    }
 

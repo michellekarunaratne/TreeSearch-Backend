c8fcf7561dd119d00b815245ee28447f72867949
Michelle <msumalini@gmail.com>
2019-12-27 12:19:22 +0530

    fixing CORS issue

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ const dbString=require('./db-Config');
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN || "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   if ('OPTIONS' == req.method) {

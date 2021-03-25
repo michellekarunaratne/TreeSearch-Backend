888f282054878342ab6daebde6affa6f625cf3eb
Madz96 <mad.subasinghe@gmail.com>
2021-03-25 23:40:28 +0530

    new db string

--- db-Config.js
+++ db-Config.js
@@ -1,3 +1,8 @@
+// module.exports = {
+//     db: 'mongodb+srv://driver-1:driver_1@treesearch-of8hf.mongodb.net/tree-search-db?retryWrites=true&w=majority'
+//  };
+
 module.exports = {
-    db: 'mongodb+srv://driver-1:driver_1@treesearch-of8hf.mongodb.net/tree-search-db?retryWrites=true&w=majority'
- };
+	db:
+		"mongodb+srv://madhawa:tree22%4022@cluster0.lnfn9.mongodb.net/treeSearch?authSource=admin&replicaSet=atlas-5fvkw8-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+};

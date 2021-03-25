b19bfaec9e34868b5481e5b9ee20e1800bb767f6
Madz96 <mad.subasinghe@gmail.com>
2021-03-25 23:37:12 +0530

    new structure and queries for trees

--- controllers/treeController.js
+++ controllers/treeController.js
@@ -19,33 +19,9 @@ function retrieveAllTrees(){
 
 function retrieveTreeResults(filterObject){
 
-
-    if("diameter" in filterObject){
-        var querydbh=filterObject.dbh;
-        filterObject["dbh.0"]={"$lte":querydbh}
-        filterObject["dbh.1"]={"$gte":querydbh}
-        delete filterObject["dbh"]
-    }
-
-    if("height" in filterObject){
-         var queryheight=filterObject.height;
-        filterObject["$and"]=[{"max_height":{"$gte":queryheight[0]}},{"max_height":{"$lte":queryheight[1]}}]
-        delete filterObject["height"]
-    }
-
-    if("use" in filterObject){
-        var queryuse=filterObject.use;
-        filterObject["use"]={"$in":[queryuse]}
-    }
-
     if("tree_size" in filterObject){
         var querytree_size=filterObject.tree_size;
         filterObject["tree_size"]={"$in":[querytree_size]}
-    }
-
-    if("taxonomic_status" in filterObject){
-        var querytaxonomic_status=filterObject.taxonomic_status;
-        filterObject["taxonomic_status"]={"$in":[querytaxonomic_status]}
     }
 
     if("climate" in filterObject){
@@ -53,9 +29,9 @@ function retrieveTreeResults(filterObject){
         filterObject["climate"]={"$in":[queryclimate]} 
     }
 
-    if("elevation" in filterObject){
+    if("suitable_location" in filterObject){
         var queryelevation=filterObject.elevation;
-        filterObject["elevation"]={"$in":[queryelevation]} 
+        filterObject["suitable_location"]={"$in":[queryelevation]} 
     }
     
     var promise = new Promise(function(resolve,reject){
--- data_models/tree.js
+++ data_models/tree.js
@@ -1,26 +1,21 @@
-const mongoose=require('mongoose');
-const Schema =mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-var treeSchema= new Schema(
-    {
-        family:String,
-        botanical_name :String,
-        common_name :String,
-        taxonomic_status :[String],
-        tree_size :[String],
-        max_height :Number,
-        diameter :[Number],
-        growth_rate :String,
-        use :[String],
-        water_preference :String,
-        light_preference :String,
-        soil :String,
-        habitat :String,
-        climate:[String],
-        elevation:[String]
-    }
-);
+var treeSchema = new Schema({
+	family: String,
+	botanical_name: String,
+	common_name: String,
+	conservation_status: String,
+	tree_size: [ String ],
+	height: String,
+	diameter: String,
+	climate: [ String ],
+	elevation: String,
+	suitable_location: [ String ],
+	engineering_benefits: String,
+	growth_rate: String,
+	use: String,
+	light: String
+});
 
-module.exports= mongoose.model('Tree',treeSchema);
-
-
+module.exports = mongoose.model("Tree", treeSchema);
